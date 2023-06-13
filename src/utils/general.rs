13ff026b9d32e19ad1b3b0ac34e1d9763c2e53ef
Breyner Mola <breinerm@blumer.cc>
2023-06-13 08:58:13 -0500

    can_view_post restore

--- src/utils/general.rs
+++ src/utils/general.rs
@@ -1,5 +1,5 @@
+use blumer_lib_authorization_rs::clients::post::Post;
 use blumer_lib_authorization_rs::clients::post::PostAuthorization;
-use blumer_lib_authorization_rs::clients::post::{Post, PostData};
 use blumer_lib_errors::AppError;
 use uuid::Uuid;
 
@@ -9,11 +9,11 @@ pub fn comment_description_max_len(comment: &str) -> bool {
 }
 
 pub async fn can_view_post(
-    mut _post_client: PostAuthorization,
+    mut post_client: PostAuthorization,
     post_id: Uuid,
-    _user_id: Uuid,
+    user_id: Uuid,
 ) -> Result<Post, AppError> {
-    /*let can_view_post: Post = post_client
+    let can_view_post: Post = post_client
         .can_view_post(post_id, user_id)
         .await
         .map_err(|e| {
@@ -26,13 +26,6 @@ pub async fn can_view_post(
             reason: can_view_post.reason,
             code: "CAN_NOT_SEE_POST".to_owned(),
         });
-    }*/
-    Ok(Post {
-        valid: true,
-        reason: "ok".to_owned(),
-        data: Some(PostData {
-            post_id: post_id,
-            owner_id: Uuid::default(),
-        }),
-    })
+    }
+    Ok(can_view_post)
 }
